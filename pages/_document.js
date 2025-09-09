99addadce669cbdcf6a3db5e1fd949c705b9b9ab
Collectif citoyen <ouaisfieux@gmail.com>
2025-09-09 21:43:21 +0200

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head />
+      <body style={{ fontFamily: 'Inter, sans-serif' }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
